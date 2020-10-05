1bc8fd387332023117b0b9fd02bf217f7274b160
Aman Sharma <amanrazdan@amp.dev>
2020-10-06 00:27:41 +0530

    change wordpress plugin name

--- Wordpress Plugin/monday-twimbit.php
+++ Wordpress Plugin/monday-twimbit.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Monday Integration
+Plugin Name: Monday.com Integration
 Plugin URI: https://github.com/twimbit/monday-wordpress-integration
 Description: Automated WordPress by integrating with Monday.
 Version: 0.1.1
